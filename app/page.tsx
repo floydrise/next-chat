41966bc923542e1e4ed1d47699a5127935c8c9bf
Stefan <floydrise@gmail.com>
2025-03-06 17:45:30 +0200

    Setup supabase

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,26 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 import { ThemeToggle } from "@/components/ThemeToggle";
 
 export default function Home() {
   return (
     <div className={"max-w-3xl mx-auto md:py-10 h-screen"}>
-      <div className={"h-full border rounded-md"}></div>
+      <div className={"h-full border rounded-md"}>
+        <div className={"p-5 border-b flex justify-between items-center"}>
+          <div>
+            <h1 className={"font-bold text-xl"}>Daily Chats</h1>
+            <div className={"flex items-center gap-2"}>
+              <div
+                className={"bg-green-400 rounded-full h-3 w-3 animate-pulse"}
+              ></div>
+              <p className={"text-sm text-gray-500"}>Two online</p>
+            </div>
+          </div>
+          <div className={"flex gap-2"}>
+            <ThemeToggle />
+            <Button>Log in</Button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
